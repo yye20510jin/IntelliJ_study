622dd920d1fe738275295ca7c7df7f38914901df
yye20510jin <yye20510@naver.com>
2025-11-12 15:58:59 +0900

    Mariadb연결

--- src/main/java/com/example/test/DemoApplication.java
+++ src/main/java/com/example/test/DemoApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(
-        exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class }
-)
+//@SpringBootApplication(
+//        exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class }
+//)
+
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
