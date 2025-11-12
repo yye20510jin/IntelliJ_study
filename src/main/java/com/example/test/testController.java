ded66d440088cdc6122ac8b594f1c867b3e98884
yye20510jin <yye20510@naver.com>
2025-11-12 13:51:06 +0900

    작동 테스트.

---
+++ src/main/java/com/example/test/testController.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @GetMapping("/")
+    public String home(){
+        return "test success";
+    }
+}
